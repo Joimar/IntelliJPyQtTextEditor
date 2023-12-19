4ec0121f0e684d8c570fb086de4d950d155ce786
Joimar <joimarbgf@gmail.com>
2023-12-18 22:29:12 -0300

    saving file function checks if file exists, if so, it overwittes it

--- UIClasses/Window.py
+++ UIClasses/Window.py
@@ -2,7 +2,7 @@
 import sys
 
 from PySide6.QtWidgets import QApplication, QMainWindow
-
+import os.path
 from UIFiles.UIMainWindow import Ui_MainWindow
 
 
@@ -11,7 +11,28 @@ class MainWindow(QMainWindow):
         super().__init__(parent)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(self.pressButton)
+        self.ui.actionNew.triggered.connect(self.pressFileNew)
+        self.ui.menuFile.triggered.connect(self.pressFile)
+        self.ui.actionSave.triggered.connect(self.saveFile)
+        self.ui.actionSave.triggered.connect(self.ui.plainTextEdit.textChanged)
 
-    def pressButton(self):
-        self.ui.pushButton.setText("Clicou!!!")
+    def pressFileNew(self):
+        print("Novo arquivo")
+
+    def pressFile(self):
+        print("File")
+
+    def pressFileSave(filename):
+        print("Save")
+
+    def saveFile(self):
+        check_file = os.path.isfile("myfile.txt")
+        if check_file:
+            f = open("myfile.txt", "w")
+            f.write(self.ui.plainTextEdit.toPlainText())
+            f.close()
+        else:
+            f = open("myfile.txt", "x")
+            f.write(self.ui.plainTextEdit.toPlainText())
+            f.close()
+
