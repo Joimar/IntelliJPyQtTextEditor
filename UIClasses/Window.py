18c60f38f2a78379b7b138a25b106ca811a11209
Joimar <joimarbgf@gmail.com>
2023-12-27 23:34:22 -0300

    fixed problem in updating current file

--- UIClasses/Window.py
+++ UIClasses/Window.py
@@ -11,30 +11,33 @@ from UIFiles.UIMainWindow import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
+    current_file = ""
+
     def __init__(self, parent=None):
         super().__init__(parent)
+
+
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.actionNew.triggered.connect(self.pressFileNew)
-        self.ui.menuFile.triggered.connect(self.pressFile)
+
         self.ui.actionSave.triggered.connect(self.pressFileSave)
         self.ui.actionSave.triggered.connect(self.ui.plainTextEdit.textChanged)
         self.ui.actionOpen.triggered.connect(self.pressFileOpen)
 
+
     def pressFileNew(self):
         print("Novo arquivo")
-        file = QFileDialog.getOpenFileName(self, 'Open file', '', 'Text files (*.txt)')
+        print(self.current_file)
 
-
+    # Open Functionalities are done
     def pressFileOpen(self):
         file = QFileDialog.getOpenFileName(self, 'Open file', '', 'Text files (*.txt)')
         print(file[0])
         f = open(file[0], "r")
         self.ui.plainTextEdit.setPlainText(f.read())
         f.close()
-    def pressFile(self):
-        print("File")
-
+        self.current_file = file[0]
     def pressFileSave(self):
         print("Save")
         #path = QFileDialog.getExistingDirectory(self, 'Select Folder')
