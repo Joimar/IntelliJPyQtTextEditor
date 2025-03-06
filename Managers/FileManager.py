db8167976c43b44d766b9ac0afc29554f684aa1f
Joimar Brito Gonçalves FIlho <joimarbgf@gmail.com>
2025-03-06 10:02:38 -0300

    FileManager.py - refactored updatingFile and read

--- Managers/FileManager.py
+++ Managers/FileManager.py
@@ -21,7 +21,7 @@ class FileManager:
 
     @staticmethod
     def updatingFile(file_path, content):
-
+        """Overwrite the content of an existing file."""
         try:
             with open(file_path, "w") as f:
                 # f.truncate(0)
@@ -29,18 +29,26 @@ class FileManager:
                 # f.close()
 
         except FileExistsError as error:
-            print(f"Error when trying to update file: {error}")
+            print(f"Error when trying to update file: {error}.")
 
     @staticmethod
-    def read(name):
-
+    def read(file_path):
+        """Read the content of a file and returns it as string. In case of error, returns None"""
         try:
-            f = open(name, "r")
-            content = f.read()
-            f.close()
-            return content
+            with open(file_path, "r") as f:
+                # f = open(file_path, "r")
+                # content = f.read()
+                # f.close()
+                return f.read()
+        except FileNotFoundError:
+            print(f"Error: The file '{file_path}' was not found.")
+            return None
         except FileExistsError as error:
-            print(error)
+            print(f"Error: file does not exist: {error}.")
+            return None
+        except Exception as error:
+            print(f"Error: Not possible to read the file: {error}")
+            return None
 
     @staticmethod
     def append(name, content):
