c97b7375121e9eaf4c781ee77a587a204df3bf6a
Joimar Brito Gonçalves FIlho <joimarbgf@gmail.com>
2025-10-17 14:48:52 -0300

    added en_US.ts

---
+++ Utils/AppStrings.py
@@ -0,0 +1,29 @@
+class AppStrings:
+    # MainWindow
+    WINDOW_TITLE = ("MainWindow", "Text Editor")
+    OPEN_FILE = ("MainWindow", "Open file")
+    SAVING_FILE = ("MainWindow", "Saving File")
+    EXPORT_PDF = ("MainWindow", "Export PDF")
+    EXPORT_COMPLETED = ("MainWindow", "Export Completed")
+
+    # ExportDialog
+    FILE_SAVED = ("ExportDialog", "File saved in {0}")
+    EXPORT_ERROR = ("ExportDialog", "Error of Exporting")
+
+    # Close Event
+    PROGRAM_NAME = ("MainWindow", "Program Name")
+    SAVE_CHANGES_QUESTION = ("MainWindow", "Do you want to save the changes?")
+
+    # Menus (já estão na UI, mas se precisar acessar no código)
+    MENU_FILE = ("MainWindow", "File")
+    MENU_EDIT = ("MainWindow", "Edit")
+    MENU_APPEARANCE = ("MainWindow", "Appearance")
+    MENU_LANGUAGE = ("MainWindow", "Language")
+
+    # Actions
+    ACTION_NEW = ("MainWindow", "New")
+    ACTION_OPEN = ("MainWindow", "Open")
+    ACTION_SAVE = ("MainWindow", "Save")
+    ACTION_SAVE_AS = ("MainWindow", "Save as")
+    ACTION_UNDO = ("MainWindow", "Undo")
+    ACTION_REDO = ("MainWindow", "Redo")
